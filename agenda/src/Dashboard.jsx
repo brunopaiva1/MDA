c5186fc94204cbe50c334da96d9d499eba7a48f0
brunopaiva1 <brunopaivamaster10@gmail.com>
2025-07-25 20:32:06 -0300

    <refactor>: mv borders for small

--- agenda/src/Dashboard.jsx
+++ agenda/src/Dashboard.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import './Dashboard.css';
 import { Plus } from 'lucide-react';
-
-import logoEmpresa from './assets/1.png';
+import logoEmpresa from './assets/logo-mda.png';
 
 function Dashboard() {
-
   const handleAddTaskClick = () => {
     alert('Botão de adicionar tarefa clicado!');
   };
 
   return (
     <div className="dashboard-menu-container">
-      <img src={logoEmpresa} alt="Logo da Empresa" className="company-logo" />
-      
-      <h1 className="menu-title">Minhas Tarefas</h1>
-      
-      <button className="add-task-button" onClick={handleAddTaskClick}>
-        <Plus size={40} />
-      </button>
+      <div className="content-wrapper">
+        <img src={logoEmpresa} alt="Logo da Empresa" className="company-logo" />
+        
+        <h1 className="menu-title">Minhas Tarefas</h1>
+        
+        <button className="add-task-button" onClick={handleAddTaskClick}>
+          <Plus size={40} />
+        </button>
+      </div>
     </div>
   );
 }
